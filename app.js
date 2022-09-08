402000360b3e1270645a1550a1e323ed3d50dfd6
Gbose(eezzy) <eezzy2k3@yahoo.com>
2022-09-08 05:05:12 +0100

    added security

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ const cartRouter = require("./src/routes/cartRoute")
 
 const orderRouter = require("./src/routes/orderroute")
 
+const mongoSanitize = require('express-mongo-sanitize')
+
+const helmet = require("helmet")
+
+const xss = require("xss-clean")
+
+const rateLimit = require('express-rate-limit')
+
+const hpp = require("hpp")
+
+const cors = require("cors")
+
 
 const colors = require("colors")
 
@@ -31,6 +43,29 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieparser())
 
 connectDb()
+
+// sanitize data
+app.use(mongoSanitize())
+
+// set security headers
+app.use(helmet())
+
+// prevent xss attack
+app.use(xss())
+
+// set rate limit
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 150, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
+// allow cors
+app.use(cors())
+
+app.use(hpp())
 
 // mount routers
 app.use("/api/v1/auth",authRouter)
